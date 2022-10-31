697d9b67fa39773cd21b121a93fef52bec68ce5f
pascual_silvestri <pascualsilvestri14@gmail.com>
2022-10-31 00:45:42 -0300

    termiando semana 5

---
+++ juego/Fondos/StarFieldBackground.gd
@@ -0,0 +1,12 @@
+extends ParallaxBackground
+tool
+
+export var color_fondo:Color = Color.black
+
+func _ready() -> void:
+	$ColorRect.color = color_fondo
+
+
+func _process(delta: float) -> void:
+	if Engine.editor_hint:
+		$ColorRect.color = color_fondo
